b84de3c7d05e759a3b168cad7f9ef88bda385619
sidharth.r <sidharth.r@nutanix.com>
2021-12-20 11:40:40 +0530

    cmsp test serice

--- src/main/java/com/sidh/springboot/testqueryextraction/repo/EmployeeRepo.java
+++
@@ -1,8 +0,0 @@
-package com.sidh.springboot.testqueryextraction.repo;
-
-import com.sidh.springboot.testqueryextraction.model.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepo  extends CrudRepository<Employee, Integer> {
-
-}

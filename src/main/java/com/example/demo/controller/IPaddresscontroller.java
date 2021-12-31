6fe2ce2b146253d982f6b302ba62442b76628dad
hubsid <sidharth.r@nutanix.com>
2021-12-31 10:44:29 +0530

    changing root context ip address to /cts

--- src/main/java/com/example/demo/controller/IPaddresscontroller.java
+++ src/main/java/com/example/demo/controller/IPaddresscontroller.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/cts")
 public class IPaddresscontroller {
 
     private RestTemplate restTemplate = new RestTemplate();
